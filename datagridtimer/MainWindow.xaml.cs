eadabc2d242cfb315528074d23b883e6e6334bb3
uzair <'uzairanwar2299@gmail.com'>
2021-12-27 10:49:14 +0500

    Add project files.

---
+++ datagridtimer/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace datagridtimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Users = new List<UserTab>();
+            UserTab user = new UserTab();
+            this.Users = user.GetUsers();
+            dgSimple.ItemsSource = this.Users;
+
+            this.StartMockValue();
+
+            this.StartRefreshDataGrid();
+        }
+
+        private List<UserTab> Users;
+
+        private void StartMockValue()
+        {
+            // NOTE: this is a thread to mock the business logic, it change 'Time' value.
+            Task.Run(() =>
+            {
+                var rando = new Random();
+                while (true)
+                {
+                    foreach (var user in this.Users)
+                    {
+                            user.Time = user.Time + 1;
+                    }
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+
+        public void StartRefreshDataGrid()
+        {
+            // NOTE: this is a thread to update data grid.
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    foreach (var user in this.Users)
+                    {
+                        // NOTE: update if the time changed.
+                        if (user.DisplayTime != user.Time)
+                        {
+                            user.DisplayTime = user.Time;
+                        }
+                    }
+
+                    // NOTE: refresh the grid every seconds.
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            string input = TxtSearch.Text;
+            foreach (var mc in this.Users)
+            {
+                if(mc.AgentName.ToLower() == input.ToLower())
+                {
+                    mc.Time = 0;
+                }
+            }
+        }
+    }
+
+
+}  
---
+++ datagridtimer/UserTab.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datagridtimer
+{
+    public class UserTab : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string AgentName { get; set; }
+        public string Extension { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string RowDetail { get; set; }
+        public int ActiveParticipants { get; set; }
+        public int HeldParticipants { get; set; }
+        public int Duration { get; set; }
+        public string CallStatus { get; set; }
+        public string QueueName { get; set; }
+
+        // NOTE: this is the 'real time', some business thread will update the value.
+        public int Time { get; set; }
+
+        private int displayTime;
+
+        // NOTE: this is the 'displayed time' in the DataGrid.
+        public int DisplayTime
+        {
+            get { return displayTime; } 
+            set
+            {
+                displayTime = value;
+                this.OnPropertyChange(nameof(this.DisplayTimeString));
+            }
+            }
+
+
+        public string DisplayTimeString => $"{(displayTime / 60 / 60).ToString().PadLeft(2,'0')}:{(displayTime / 60 % 60).ToString().PadLeft(2,'0')}:{(displayTime % 60).ToString().PadLeft(2, '0')}";
+
+        public string CallStatusColor { get; set; }
+        public bool isRowDetail { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void OnPropertyChange(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        public List<UserTab> GetUsers()
+        {
+            var maxSeconds = 24 * 60 * 60;
+            var random = new Random();
+
+
+            List<UserTab> users = new List<UserTab>();
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 1, AgentName = "John Doe", Extension = "42033", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new UserTab() { CallStatusColor = "#d70d0d", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Not Ready - Lunch", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 3, AgentName = "John Doe", Extension = "42034", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+            users.Add(new UserTab() { CallStatusColor = "#E6A30C", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = true, CallStatus = "Talking", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 1, AgentName = "John Doe", Extension = "42035", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new UserTab() { CallStatusColor = "#E6A30C", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = true, CallStatus = "Talking", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 2, AgentName = "AJohn Doe", Extension = "42036", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new UserTab() { CallStatusColor = "#E6A30C", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = true, CallStatus = "Talking", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 3, AgentName = "BJohn Doe", Extension = "42037", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+            users.Add(new UserTab() { CallStatusColor = "#E6A30C", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = true, CallStatus = "Talking", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 1, AgentName = "CJohn Doe", Extension = "42038", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new UserTab() { CallStatusColor = "#E6A30C", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = true, CallStatus = "Talking", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 2, AgentName = "DJohn Doe", Extension = "42039", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new UserTab() { CallStatusColor = "#E6A30C", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = true, CallStatus = "Talking", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 3, AgentName = "EJohn Doe", Extension = "42003", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 1, AgentName = "EJohn Doe", Extension = "42053", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 2, AgentName = "FJohn Doe", Extension = "42073", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 3, AgentName = "GJohn", Extension = "42078", Name = "Sammy", Birthday = new DateTime(1991, 9, 2) });
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 1, AgentName = "HJohn Doe", Extension = "42083", Name = "John Doe", Birthday = new DateTime(1971, 7, 23) });
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 2, AgentName = "IJohn Doe", Extension = "42093", Name = "Jane Doe", Birthday = new DateTime(1974, 1, 17) });
+            users.Add(new UserTab() { CallStatusColor = "#1BDA6D", DisplayTime = random.Next(0, maxSeconds), QueueName = "AMQ", isRowDetail = false, CallStatus = "Ready", Duration = 1000, HeldParticipants = 500, ActiveParticipants = 30, Id = 3, AgentName = "JJohn Doe", Extension = "42013", Name = "Sammy Doe", Birthday = new DateTime(1991, 9, 2) });
+            return users;
+        }
+    }
+}
